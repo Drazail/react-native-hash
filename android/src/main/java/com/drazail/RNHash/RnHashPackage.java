b603e3307f8cfccf0e4cfeba2f85bc612101afa4
Drazail <reza.y.3@gmail.com>
2022-05-08 20:05:49 +0300

    upgrading to gradel 7.1.3

--- android/src/main/java/com/drazail/RNHash/RnHashPackage.java
+++ android/src/main/java/com/drazail/RNHash/RnHashPackage.java
@@ -1,7 +1,6 @@
 package com.drazail.RNHash;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -15,12 +14,6 @@ public class RnHashPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RnHashModule(reactContext));
     }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return null;
-    }
-
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
