667e7a823d7613cce417d46b6d88da355058b5dc
Drazail <reza.y.3@gmail.com>
2022-05-08 18:01:43 +0300

    updating project to work with newer versions of react-native, gradle and JUnit

--- android/src/main/java/com/drazail/RNHash/RnHashPackage.java
+++ android/src/main/java/com/drazail/RNHash/RnHashPackage.java
@@ -1,6 +1,7 @@
 package com.drazail.RNHash;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -13,6 +14,11 @@ public class RnHashPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RnHashModule(reactContext));
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return null;
     }
 
 
--- android/src/main/java/com/drazail/RNHash/Utils/RejectionExceptions.java
+++ android/src/main/java/com/drazail/RNHash/Utils/RejectionExceptions.java
@@ -4,11 +4,8 @@ import com.facebook.react.bridge.Promise;
 
 public class RejectionExceptions extends Exception {
 
-    private String code;
-
     public RejectionExceptions(String code, String message) {
         super(message);
-        this.code = code;
     }
 
     public static void rejectFileIsDirectory(Promise promise) {
